576233f8b7237a4976f2bcb8fb205dfd3f7d7926
Aoi-00 <leehongying11@gmail.com>
2022-01-28 01:22:55 +0800

    clean up

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,10 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonImg, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol } from '@ionic/react';
 import React, { Component, useState } from 'react';
-import usePhotoGallery from '../hooks/usePhotoGallery';
 import { camera, trash, close } from 'ionicons/icons';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 
 
 const Home = () => {
-  //const { photos, takePhoto } = usePhotoGallery();
   const [photos, setPhotos] = useState<any>([]);
   const takePhoto = async () => {
     const photo = Camera.getPhoto({
@@ -26,13 +24,6 @@ const Home = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        {/* <IonRow>
-          {photos.map((photo, index) => (
-            <IonCol size="6" key={index}>
-              <IonImg src={photo.webviewPath} />
-            </IonCol>
-          ))}
-        </IonRow> */}
         {photos && <IonImg src={photos}/>}
         <IonFab vertical="bottom" horizontal="center" slot="fixed">
           <IonFabButton onClick={() => takePhoto()}>
