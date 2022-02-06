db345efb7853a613aaedc41f56186d7aa7b0b438
Aoi-00 <leehongying11@gmail.com>
2022-02-06 21:02:30 +0800

    base64String

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,42 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonImg, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol } from '@ionic/react';
 import React, { Component, useState } from 'react';
-import { camera, trash, close } from 'ionicons/icons';
+import { camera } from 'ionicons/icons';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 
+const postImg = (postData : any) => (dispatch:any) => {
+  fetch('http://127.0.0.1:5000/', {
+      method: 'POST',
+      headers: {
+          'content-type': 'application/json'
+      },
+      body: JSON.stringify(postData)
+  })
+      .then(res => {res.json(); console.log(res)})
+      .then(data => console.log(data))
+      // .then(data => {
+      //     dispatch({
+      //         type: 'PREDICTION',
+      //         payload: data
+      //     })
+      // })
+}
 
 const Home = () => {
-  const [photos, setPhotos] = useState<any>([]);
+  const [photos, setPhotos] = useState<any>();
   const takePhoto = async () => {
     const photo = Camera.getPhoto({
-      resultType: CameraResultType.Uri,
+      resultType: CameraResultType.Base64,
       source: CameraSource.Camera,
       quality: 100,
+    }).then(photo=>{
+      
+      let image_url = `data:image/jpeg;base64,${photo.base64String}`;
+      setPhotos(image_url)
+      console.log(photo.base64String)
+      postImg({
+        image: photo.base64String
+      })
     });
-    let image_url = `data:image/jpeg:base64,${(await photo).base64String}`;
-    setPhotos(image_url)
   };
 
   return <div>
