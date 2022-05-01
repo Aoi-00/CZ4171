929bb4ada0b825ee71c1d36c063e5f71040e0c05
Aoi-00 <leehongying11@gmail.com>
2022-05-01 18:13:41 +0800

    Final submission for CZ4171

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,41 +1,39 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonImg, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonImg, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonToast } from '@ionic/react';
 import React, { Component, useState } from 'react';
-import { camera } from 'ionicons/icons';
+import { camera, send } from 'ionicons/icons';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 
-const postImg = (postData : any) => (dispatch:any) => {
-  fetch('http://127.0.0.1:5000/', {
+const Home = () => {
+  const postImg = () => {
+    let postData = {
+      'image': data
+    }
+    fetch('http://192.168.18.220:5000/', {
       method: 'POST',
       headers: {
-          'content-type': 'application/json'
+        'content-type': 'application/json'
       },
       body: JSON.stringify(postData)
-  })
-      .then(res => {res.json(); console.log(res)})
-      .then(data => console.log(data))
-      // .then(data => {
-      //     dispatch({
-      //         type: 'PREDICTION',
-      //         payload: data
-      //     })
-      // })
-}
+    })
+      .then(res => res.json())
+      .then((results) => {
+        setResults(results)
+      })
+  }
 
-const Home = () => {
   const [photos, setPhotos] = useState<any>();
+  const [data, setData] = useState<any>();
+  const [results, setResults] =useState<any>({ Results: '', Probability: ''});
+  const [showToast1, setShowToast1] = useState(false);
   const takePhoto = async () => {
     const photo = Camera.getPhoto({
       resultType: CameraResultType.Base64,
       source: CameraSource.Camera,
       quality: 100,
-    }).then(photo=>{
-      
+    }).then(photo => {
       let image_url = `data:image/jpeg;base64,${photo.base64String}`;
       setPhotos(image_url)
-      console.log(photo.base64String)
-      postImg({
-        image: photo.base64String
-      })
+      setData(photo.base64String)
     });
   };
 
@@ -47,11 +45,26 @@ const Home = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        {photos && <IonImg src={photos}/>}
+        {photos && <IonImg src={photos} />}
         <IonFab vertical="bottom" horizontal="center" slot="fixed">
-          <IonFabButton onClick={() => takePhoto()}>
-            <IonIcon icon={camera}></IonIcon>
-          </IonFabButton>
+          <IonRow>
+            <IonCol>
+              <IonFabButton onClick={() => takePhoto()}>
+                <IonIcon icon={camera}></IonIcon>
+              </IonFabButton>
+            </IonCol>
+            <IonCol>
+              <IonFabButton onClick={() => {postImg(); setShowToast1(true);}}>
+                <IonIcon icon={send}></IonIcon>
+              </IonFabButton>
+            </IonCol>
+            { results.Result !== undefined && <IonToast
+              isOpen={showToast1}
+              onDidDismiss={() => setShowToast1(false)}
+              message={`Results: ${results.Result} Probability: ${results.Probability}`}
+              duration={300}
+            />}
+          </IonRow>
         </IonFab>
       </IonContent>
     </IonPage>
